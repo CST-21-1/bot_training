57d49186e3d66db7dda526e068805652c7be6f62
Mehanari <102820106+Mehanari@users.noreply.github.com>
2022-11-16 19:36:07 +0300

    Took all strings from code to handy constants. Also moved bot token to the .env file.

--- taskBot.py
+++ taskBot.py
@@ -1,42 +1,63 @@
+import  os
 from telebot import *
 import taskBotDB
+from dotenv import load_dotenv
 
-bot = telebot.TeleBot('5741260025:AAEUYgaP6f1ULfHe9gbsViVZ_XEJn0DjhGA')
+load_dotenv()
+
+#constants
+
+greeting = "Welcome, this is tasklist bot!"
+
+addTaskMessage = 'Add new task'
+
+showTaskMessage = 'Show tasks'
+
+noFuncMessage = "No functionality for this button yet"
+
+unknownFuncMessage = "Unknown functions, please, choose only stated options!"
+
+pressButtonMessage = "Press one of the buttons"
+
+
+#start of the program
+
+TOK = os.getenv("TOKEN")
+
+bot = telebot.TeleBot(TOK)
 
 botDB = taskBotDB.BotDB()
 
-botDB.initialize()
 botDB.show_all_users()
-
 
 @bot.message_handler(commands=['start', 'help'])
 def start(message):
-	bot.reply_to(message, "Welcome, this is tasklist bot!")
+	bot.reply_to(message, greeting)
 	show_main_options(message)
 
 
 def show_main_options(message):
 	markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
-	addTaskButton = types.KeyboardButton('Add new task')
-	showTasksButton = types.KeyboardButton('Show tasks')
+	addTaskButton = types.KeyboardButton(addTaskMessage)
+	showTasksButton = types.KeyboardButton(showTaskMessage)
 	markup.row(addTaskButton, showTasksButton)
 
-	mess = bot.send_message(message.chat.id, "Press one of the buttons" ,reply_markup=markup)
+	mess = bot.send_message(message.chat.id, pressButtonMessage ,reply_markup=markup)
 	bot.register_next_step_handler(mess, process_main_options)
 
 
 def process_main_options(message):
 	messageText = message.text
-	if messageText == 'Add new task':
-		bot.send_message(message.chat.id, "No functionality for this button yet")
+	if messageText == addTaskMessage:
+		bot.send_message(message.chat.id, noFuncMessage)
 		#TODO: Make logic for receiving data from the user
 		show_main_options(message)
-	elif messageText == 'Show tasks':
+	elif messageText == showTaskMessage:
 		# TODO: Make logic for showing tasks to the user
-		bot.send_message(message.chat.id, "No functionality for this button too")
+		bot.send_message(message.chat.id, noFuncMessage)
 		show_main_options(message)
 	else:
-		mess = bot.send_message(message.chat.id, "Unknown functions, please, choose only stated options!")
+		mess = bot.send_message(message.chat.id, unknownFuncMessage)
 		show_main_options(message)
 
 
--- taskBotDB.py
+++ taskBotDB.py
@@ -2,24 +2,13 @@ import mysql.connector
 
 class BotDB:
 
-    def __int__(self):
+    def __init__(self):
         self.connection = mysql.connector.connect(
             user='root',
             host='127.0.0.1',
             password='root_15YK',
             database='taskmanagementdb'
         )
-        print("lol")
-        self.cursor = self.connection.cursor()
-
-    def initialize(self):
-        self.connection = mysql.connector.connect(
-            user='root',
-            host='127.0.0.1',
-            password='root_15YK',
-            database='taskmanagementdb'
-        )
-        print("lol")
         self.cursor = self.connection.cursor()
 
     def show_all_users(self):
